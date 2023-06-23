14e383bb731483766f084d6051c72ee3f37d1103
showman <s.howman.4580@gmail.com>
2023-06-23 14:52:50 +0800

    Get the screen dimensions and start blender up on the right hand side

--- oo2cad.sh
+++ oo2cad.sh
@@ -1,3 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-blender -P oo2cad.py
+dimen=$(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/')
+
+IFS='x'
+
+#Read the split words into an array based on space delimiter
+read -a xy <<< "$dimen"
+
+echo $dimen
+w="${xy[0]}"
+h="${xy[1]}"
+w2=$(echo "$w/2" | bc)
+blender --window-geometry $w2 0 $w2 $h -P oo2cad.py
